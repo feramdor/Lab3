9464383918e3e8605c22297f0528a970b265b81b
MaximilianoGarciaMora <31109845+MaximilianoGarciaMora@users.noreply.github.com>
2023-03-29 16:21:43 -0600

    Actualización Functions
    Nuevas funciones functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,22 @@
+"""
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: visualizations.py : python script with data visualization functions                         -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
 import pandas as pd
 import re
+import numpy as np
+
+def eliminar_espacios(cadena):
+    patron = r'\s+'
+    return re.sub(patron, '', cadena)
+def splitter(cadena):
+    lista = cadena.split()
+    return lista[0]
 
 def f_leer_archivo(param_archivo: str, param_lib = False, param_cred = dict()) -> pd.DataFrame:
     def verificar_formato_df(df):
@@ -29,6 +46,14 @@ def f_leer_archivo(param_archivo: str, param_lib = False, param_cred = dict()) -
         else:
             print("Favor de corregir el archivo")
             return None
+def f_benchmark(param_archivo: str) -> pd.DataFrame:
+    out = pd.read_csv(param_archivo)
+    out.rename(columns={"Price" : "close"}, inplace=True)
+    out['close'] = out['close'].apply(lambda x: x.replace(",",""))
+    out['close'] = out['close'].astype(float)
+    out["timestamp"] = pd.to_datetime(out["Date"]).dt.date
+    out.set_index("timestamp", inplace=True)
+    return out
 
 def f_pip_size(param_ins: str) -> int:
     try:
@@ -43,6 +68,10 @@ def f_columnas_tiempos(param_data: pd.DataFrame) -> pd.DataFrame:
     param_data["opentime"] = pd.to_datetime(param_data["opentime"])
     param_data["closetime"] = pd.to_datetime(param_data["closetime"])
     param_data["Tiempo"] = (param_data["closetime"] - param_data["opentime"]).dt.total_seconds()
+    param_data['Profit'] = param_data['Profit'].apply(eliminar_espacios).astype(float)
+    param_data['Volume'] = param_data['Volume'].apply(splitter).astype(float)
+    param_data = param_data.rename(columns={'Price' : 'openprice'})
+    param_data = param_data.rename(columns={'State' : 'closeprice'})
     return param_data
 
 def f_columnas_pips(param_data: pd.DataFrame) -> pd.DataFrame:
@@ -50,7 +79,8 @@ def f_columnas_pips(param_data: pd.DataFrame) -> pd.DataFrame:
     param_data["pips"] = (param_data["closeprice"] - param_data["openprice"]) * param_data["pip_size"]
     param_data["pips"] = param_data.apply(lambda x: -x['pips'] if x['Type'] == 'sell' else x['pips'], axis=1)
     param_data['pips_acm'] = param_data['pips'].cumsum()
-    param_data['profit_acm'] = (param_data['pips'] * param_data['Volume']).cumsum()
+    param_data["profit"] = (param_data['pips'] * param_data['Volume'])
+    param_data['profit_acm'] = param_data["profit"].cumsum()
     return param_data
 
 def f_estadisticas_ba(param_data: pd.DataFrame) -> pd.DataFrame:
@@ -107,4 +137,73 @@ def f_estadisticas_ba(param_data: pd.DataFrame) -> pd.DataFrame:
         "df_2_ranking" : df_2_ranking
     }
     return ex
-#Prueba
+
+def f_evolucion_capital(df: pd.DataFrame, capital: float = 100000) -> pd.DataFrame:
+    # Transformación de timestamp
+    df['closetime'] = pd.to_datetime(df['closetime']).dt.date
+    # Cálculo del profit
+    profit_d = df.groupby('closetime')['profit'].sum()
+
+    # Acumulado diario 
+    profit_acm_d = (profit_d.cumsum() + capital)
+
+    # Salida
+    ex = pd.DataFrame({
+        'timestamp' : profit_d.index,
+        'profit_d' : profit_d,
+        'profit_acm_d' : profit_acm_d
+    })
+    ex.set_index('timestamp',inplace=True)
+    return ex
+
+def f_estadisticas_mad(mad: pd.DataFrame, benchmark: pd.DataFrame, rf: float = 0.05) -> pd.DataFrame:
+    mad['timestamp'] = mad.index
+    # Calcular el logaritmo de los rendimientos diarios
+    log_rendimientos = np.log(mad['profit_acm_d'] / mad['profit_acm_d'].shift(1))
+    # Calcular el promedio y la desviación estándar de los rendimientos diarios
+    # Calcular el Sharpe Ratio utilizando la fórmula
+    sharpe_ratio = (log_rendimientos.mean() - rf) / log_rendimientos.std()
+    benchmark = benchmark.reindex(mad.index)
+    log_benchmark = np.log(benchmark['close'] / benchmark['close'].shift(1))
+    log_rendimientos_diferencia = log_rendimientos - log_benchmark
+    sharpe_actualizado = (log_rendimientos.mean() - log_benchmark.mean()) / log_rendimientos_diferencia.std()
+    ##################################### Esbozo del drawup y drawdown
+    # Obtenemos el máximo de capital acumulado hasta la fecha
+    mad['max_capital'] = mad['profit_acm_d'].cummax()
+        # Calculamos el drawdown
+    mad['drawdown'] = (mad['max_capital'] - mad['profit_acm_d'])/mad['max_capital']
+        # Obtenemos la fecha de inicio y fin del drawdown
+    drawdown_start = mad.loc[mad['drawdown'].idxmax(), 'timestamp']
+    try:
+        drawdown_end = mad.loc[mad['drawdown'].idxmax():, 'timestamp'].loc[mad['drawdown'] == 0].iloc[0]
+    except:
+        drawdown_end = mad.loc[mad['drawdown'].idxmax():, 'timestamp'].iloc[0]
+        # Calculamos el capital perdido en el drawdown
+    try:
+        drawdown_capital = (mad.loc[mad['drawdown'].idxmax(), 'profit_acm_d'] - mad.loc[mad['drawdown'].idxmax():, 'profit_acm_d'].loc[mad['drawdown'] == 0].iloc[0])
+    except:
+        drawdown_capital = (mad.loc[mad['drawdown'].idxmax():, 'profit_acm_d'].iloc[0] - mad.loc[mad['drawdown'].idxmax(), 'profit_acm_d'])
+    # Obtenemos la fecha de inicio y fin del drawup
+    try:
+        drawup_start = mad.loc[mad['drawdown'].idxmax():, 'timestamp'].loc[mad['drawdown'] == 0].iloc[0]
+    except:
+        drawup_start = mad.loc[mad['drawdown'].idxmin(), 'timestamp']
+    try:
+        drawup_end = mad.loc[mad['drawdown'].idxmax():, 'timestamp'].loc[mad['drawdown'] == 0].iloc[-1]
+    except:
+        drawup_end = mad.loc[mad['drawdown'].idxmax():, 'timestamp'].iloc[-1]
+    # Calculamos el capital ganado en el drawup
+    try:
+        drawup_capital = (mad.loc[mad['drawdown'].idxmax():, 'profit_acm_d'].iloc[-1] - mad.loc[mad['drawdown'].idxmax(), 'profit_acm_d'])
+    except:
+        mad.loc[mad['drawdown'].idxmax():, 'profit_acm_d'].values[-1] - mad.loc[mad['drawdown'].idxmax(), 'profit_acm_d']
+    mad_statistics = pd.DataFrame(columns=['metrica','unidad','valor','descripcion'])
+    mad_statistics.loc[0] = ['sharpe_original','Cantidad',sharpe_ratio,'Sharpe Ratio Fórmula Original']
+    mad_statistics.loc[1] = ['sharpe_actualizado','Cantidad',sharpe_actualizado,'Sharpe Ratio Fórmula Ajustada']
+    mad_statistics.loc[2] = ['drawdown_capi','Fecha Inicial',drawdown_start,'Fecha inicial del DrawDown de Capital']
+    mad_statistics.loc[3] = ['drawdown_capi','Fecha Final',drawdown_end,'Fecha final del DrawDown de Capital']
+    mad_statistics.loc[4] = ['drawdown_capi','DrawDown $ (capital)',drawdown_capital,'Máxima pérdida flotante registrada']
+    mad_statistics.loc[5] = ['drawup_capi','Fecha Inicial',drawup_start,'Fecha inicial del DrawUp de Capital']
+    mad_statistics.loc[6] = ['drawup_capi','Fecha Final',drawup_end,'Fecha final del DrawUp de Capital']
+    mad_statistics.loc[7] = ['drawup_capi','DrawDown $ (capital)',drawup_capital,'Máxima ganancia flotante registrada']
+    return mad_statistics
