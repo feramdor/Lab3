2a23ac3d0e00945111d735c889953a7eca0e6527
MaximilianoGarciaMora <31109845+MaximilianoGarciaMora@users.noreply.github.com>
2023-03-31 07:51:40 -0600

    Actualizacion main.py

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ tab_rank["df_1_tabla"]
 tab_rank["df_2_ranking"]
 mad = functions.f_evolucion_capital(f)
 benchmark = functions.f_benchmark("files/sp500.csv")
-mad_statistics = functions.f_estadisticas_mad(mad,benchmark)
-visualizations.ranking_vis(tab_rank["df_2_ranking"])
+mad_statistics, mad_hist = functions.f_estadisticas_mad(mad,benchmark)
+behavioural_finance, DE_oc = functions.f_behavioural_finance(f)
+graph1 = visualizations.ranking_vis(tab_rank["df_2_ranking"])
+graph2 = visualizations.drawdwn_vis(mad_hist, mad_statistics)
